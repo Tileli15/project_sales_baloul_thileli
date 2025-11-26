f2b1b1b8810b3d171d9ca1eb7baa17ff58418775
thileli <tthileli285@gmail.com>
2025-11-26 11:39:15 +0100

    I have successfully created the 'project_sales_baloul_thileli' folder and set up the Conda environment 'sales_project_env'. I installed the four libraries: NumPy, Pandas, Matplotlib, and Seaborn.

--- utils.py
+++ utils.py
@@ -1,7 +1,5 @@
 import numpy as np
 
 def generate_random_sales(min_val, max_val, size):
-    """
-    Returns a NumPy array of random integers between min_val and max_val (inclusive) of length size.
-    """
+    # دالة ترجع مصفوفة أعداد صحيحة عشوائية بين القيم المطلوبة
     return np.random.randint(min_val, max_val + 1, size)
